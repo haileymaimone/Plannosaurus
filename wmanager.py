b1189cb6745bb250b73e3861d905d63058e70286
Hailey Maimone <98764633+haileymaimone@users.noreply.github.com>
2022-04-19 11:48:54 -0400

    changed database to sqlite3 for app conversion

--- wmanager.py
+++ wmanager.py
@@ -2,8 +2,7 @@ from sdates import StartingDates
 import config
 from kivymd.uix.list import OneLineAvatarIconListItem
 from kivy.uix.image import AsyncImage, Image
-import psycopg2
-import psycopg2.extras
+import sqlite3
 import datetime
 from datetime import *
 from kivy.uix.screenmanager import ScreenManager
@@ -156,103 +155,6 @@ class WindowManager(ScreenManager):
             self.ids.day7.text = "[color=#42f58d]"+ theDays.day7.strftime("%d") +"[/color]"
 
         return "main_sc"
-
-
-
-    def event_add(self, root, time):
-        timesArr = ["6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM"]
-        militaryArr = ["06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00"]
-        index = timesArr.index(time)
-        timeObj = datetime.strptime(militaryArr[index], '%H:%M').strftime("%H:%M")
-
-        if index == 0:
-            messageText = self.ids.contentEvent.text
-            self.ids.contentEvent.disabled = True
-            self.ids.add6AM.disabled = True
-        elif index == 1:
-            messageText = self.ids.sevenAM.text
-            self.ids.sevenAM.disabled = True
-            self.ids.add7AM.disabled = True
-        elif index == 2:
-            messageText = self.ids.eightAM.text
-            self.ids.eightAM.disabled = True
-            self.ids.add8AM.disabled = True
-        elif index == 3:
-            messageText = self.ids.nineAM.text
-            self.ids.nineAM.disabled = True
-            self.ids.add9AM.disabled = True
-        elif index == 4:
-            messageText = self.ids.tenAM.text
-            self.ids.tenAM.disabled = True
-            self.ids.add10AM.disabled = True
-        elif index == 5:
-            messageText = self.ids.elevenAM.text
-            self.ids.elevenAM.disabled = True
-            self.ids.add11AM.disabled = True
-        elif index == 6:
-            messageText = self.ids.noon.text
-            self.ids.noon.disabled = True
-            self.ids.add12PM.disabled = True
-        elif index == 7:
-            messageText = self.ids.onePM.text
-            self.ids.onePM.disabled = True
-            self.ids.add1PM.disabled = True
-        elif index == 8:
-            messageText = self.ids.twoPM.text
-            self.ids.twoPM.disabled = True
-            self.ids.add2PM.disabled = True
-        elif index == 9:
-            messageText = self.ids.threePM.text
-            self.ids.threePM.disabled = True
-            self.ids.add3PM.disabled = True
-        elif index == 10:
-            messageText = self.ids.fourPM.text
-            self.ids.fourPM.disabled = True
-            self.ids.add4PM.disabled = True
-        elif index == 11:
-            messageText = self.ids.fivePM.text
-            self.ids.fivePM.disabled = True
-            self.ids.add5PM.disabled = True
-        elif index == 12:
-            messageText = self.ids.sixPM.text
-            self.ids.sixPM.disabled = True
-            self.ids.add6PM.disabled = True
-        elif index == 13:
-            messageText = self.ids.sevenPM.text
-            self.ids.sevenPM.disabled = True
-            self.ids.add7PM.disabled = True
-        elif index == 14:
-            messageText = self.ids.eightPM.text
-            self.ids.eightPM.disabled = True
-            self.ids.add8PM.disabled = True
-        elif index == 15:
-            messageText = self.ids.ninePM.text
-            self.ids.ninePM.disabled = True
-            self.ids.add9PM.disabled = True
-        
-
-        conn = psycopg2.connect(
-            # host = "ec2-34-205-209-14.compute-1.amazonaws.com",
-            # database = "d19re7njihace8",
-            # user = "lveasasuicarlg",
-            # password = "c372ee6ba2bc15c476bf85a8258fa444d2a51f4323b6903a1963c0c5fb118a08",
-            # port = "5432",
-            host = "localhost",
-            database = "plannodb",
-            user = "postgres",
-            password = "postgres",
-            port = "5432",
-        )
-
-        # Create a cursor
-        c = conn.cursor()
-
-        if config.store.exists('account'):
-            config.userid = config.store.get('account')['userid']
-        
-        c.execute("INSERT INTO events(dateID, timestamp, time, messageBody, userID, sticker, color) VALUES (%s, %s, %s, %s, %s, %s, %s)", (config.dateID, timeObj, time, time + " - " + messageText, config.userid, 'circle-outline', 'default'))
-        conn.commit()
-        conn.close()
 
 
     def overview_images(self, root, image1, image2):
@@ -543,147 +445,21 @@ class WindowManager(ScreenManager):
     
             self.overview_images(root, img_1, img_2)
 
-        conn = psycopg2.connect(
-            # host = "ec2-34-205-209-14.compute-1.amazonaws.com",
-            # database = "d19re7njihace8",
-            # user = "lveasasuicarlg",
-            # password = "c372ee6ba2bc15c476bf85a8258fa444d2a51f4323b6903a1963c0c5fb118a08",
-            # port = "5432",
-            host = "localhost",
-            database = "plannodb",
-            user = "postgres",
-            password = "postgres",
-            port = "5432",
-        )
+        conn = sqlite3.connect('plannodb.db')
 
         
         c = conn.cursor()
-        query = "SELECT * FROM colors WHERE userID = %s"
+        query = "SELECT * FROM colors WHERE userID = ?"
         c.execute(query, (config.userid,))
         records = c.fetchall()
         
         if records:
-            c.execute("UPDATE colors SET style = %s, userid = %s", (style, config.userid))
+            c.execute("UPDATE colors SET style = ?, userid = ?", (style, config.userid))
         else:
-            c.execute("INSERT INTO colors (style, userid) VALUES (%s, %s)", (style, config.userid))
+            c.execute("INSERT INTO colors (style, userid) VALUES (?, ?)", (style, config.userid))
 
         conn.commit()
         conn.close()
-
-    def postEvents(self, root):
-
-        if config.store.exists('account'):
-            config.userid = config.store.get('account')['userid']
-
-     #   self.ids.contentEventMain.text = '' # reset textfield to be blank
-
-        conn = psycopg2.connect(
-            # host = "ec2-34-205-209-14.compute-1.amazonaws.com",
-            # database = "d19re7njihace8",
-            # user = "lveasasuicarlg",
-            # password = "c372ee6ba2bc15c476bf85a8258fa444d2a51f4323b6903a1963c0c5fb118a08",
-            # port = "5432",
-            host = "localhost",
-            database = "plannodb",
-            user = "postgres",
-            password = "postgres",
-            port = "5432",
-        )
-
-        c = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        query = "SELECT * FROM events WHERE userID = %s AND dateID = %s"
-        c.execute(query, (config.userid, config.dateID,))
-        records = c.fetchall()
-        records.sort(key = lambda date: datetime.strptime(date[2], "%H:%M"))
-
-        # RED 1, 0, 0, 1
-        # GREEN 0, 1, 0, 1
-        # YELLOW 1, 1, 0, 1
-        # BLUE 0, 0, 1, 1
-        # PURPLE 1, 0, 1, 1
-        # DEFAULT 0, 0, 0, 1
-        self.ids['eventContainer'].clear_widgets()
-        if records:
-            for items in records:
-                self.ids['eventContainer'].add_widget(EventItemWithCheckbox(text= '[b]' + items[4] + '[/b]'))
-                self.ids['eventContainer'].children[0].ids['eventIcon'].icon = items[6]
-
-                if items[7] == "RED":
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].text_color = 1, 0, 0, 1
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].set_text_color([1, 0, 0, 1])
-                elif items[7] == "GREEN":
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].text_color = 0, 1, 0, 1
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].set_text_color([0, 1, 0, 1])
-                elif items[7] == "YELLOW":
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].text_color = 1, 1, 0, 1
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].set_text_color([1, 1, 0, 1])
-                elif items[7] == "BLUE":
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].text_color = 0, 0, 1, 1
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].set_text_color([0, 0, 1, 1])
-                elif items[7] == "PURPLE":
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].text_color = 1, 0, 1, 1
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].set_text_color([1, 0, 1, 1])
-                else:
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].text_color = 0, 0, 0, 1
-                    self.ids['eventContainer'].children[0].ids['eventIcon'].set_text_color([0, 0, 0, 1])
-
-        
-        # easiest way to do things on pageload
-
-        # load colors data
-        query = "SELECT * FROM colors WHERE userID = %s"
-        c.execute(query, (config.userid,))
-        records = c.fetchall()
-        
-        if records:
-            self.colorChanger(self, records[0][0])
-        else:
-            self.colorChanger(self, "OG")
-        
-        conn.commit()
-        conn.close()
-        
-
-        
-    
-    def delete_eventFromAdd(self, root, time, the_event_item):
-        deleteItem = ''
-        
-        if the_event_item.text[0:3] == '[b]':
-            deleteItem = the_event_item.text.split('[b]')[1].split('[/b]')[0]
-        else:
-            deleteItem = the_event_item.text.split('[s][b]')[1].split('[/b][/s]')[0]
-
-        conn = psycopg2.connect(
-            host = "ec2-34-205-209-14.compute-1.amazonaws.com",
-            database = "d19re7njihace8",
-            user = "lveasasuicarlg",
-            password = "c372ee6ba2bc15c476bf85a8258fa444d2a51f4323b6903a1963c0c5fb118a08",
-            port = "5432",
-        )
-
-        # Create a cursor
-        c = conn.cursor()
-        query = "DELETE FROM events WHERE userid = %s AND messageBody = %s"
-        c.execute(query, (config.userid, deleteItem,))
-        
-        conn.commit()
-        conn.close()
-        
-        # if records:
-        #     for items in records:
-        #         self.parent.remove_widget(the_event_item)
-                
-        
-        
-        
-        #self.parent.remove_widget(the_event_item)
-        if time == "6 AM":
-            self.ids.contentEvent.disabled = False
-            self.ids.contentEvent.text = ''
-        elif time == "7 AM":
-            self.ids.sevenAM.disabled = False
-            self.ids.sevenAM.text = ''
 
 
 class EventItemWithCheckbox(OneLineAvatarIconListItem):
