d567a7658899641c8a58ebcbc41017e2e986345e
Hailey Maimone <98764633+haileymaimone@users.noreply.github.com>
2022-04-19 11:47:44 -0400

    changed database to sqlite for app conversion

--- db.py
+++ db.py
@@ -1,50 +1,42 @@
 import config
-import psycopg2
-import psycopg2.extras
+import sqlite3
 
 class Database:
     def __init__(self):
-        self.conn = psycopg2.connect(
-            host = "localhost",
-            database = "plannodb",
-            user = "postgres",
-            password = "postgres",
-            port = "5432",
-            # host = "ec2-34-205-209-14.compute-1.amazonaws.com",
-            # database = "d19re7njihace8",
-            # user = "lveasasuicarlg",
-            # password = "c372ee6ba2bc15c476bf85a8258fa444d2a51f4323b6903a1963c0c5fb118a08",
-            # port = "5432",
-        )
+        self.conn = sqlite3.connect('plannodb.db')
 
         self.cursor = self.conn.cursor()
         self.create_tables()
 
     def create_tables(self):
+
+        # Create alarms table
+        self.cursor.execute("CREATE TABLE if not exists alarms(alarmID integer PRIMARY KEY AUTOINCREMENT, dateID VARCHAR(255), alarmTime VARCHAR(255), alarmtext VARCHAR(255), userID INTEGER REFERENCES users(userID))")
+
          # Create users table
-        self.cursor.execute("CREATE TABLE if not exists users(userID SERIAL PRIMARY KEY, email VARCHAR(255), password VARCHAR(255), firstName VARCHAR(255), lastName VARCHAR(255))")
+        self.cursor.execute("CREATE TABLE if not exists users(userID integer PRIMARY KEY AUTOINCREMENT, email VARCHAR(255), password VARCHAR(255), firstName VARCHAR(255), lastName VARCHAR(255))")
         
         # Create events table
-        self.cursor.execute("CREATE TABLE if not exists events(eventID SERIAL PRIMARY KEY, dateID VARCHAR(255), timestamp VARCHAR(255), time VARCHAR(255), messageBody VARCHAR(255), userID int REFERENCES users, sticker VARCHAR(255), color VARCHAR(255))")
+        self.cursor.execute("CREATE TABLE if not exists events(eventID integer PRIMARY KEY AUTOINCREMENT, dateID VARCHAR(255), timestamp VARCHAR(255), time VARCHAR(255), messageBody VARCHAR(255), userID INTEGER REFERENCES users(userID), sticker VARCHAR(255), color VARCHAR(255))")
 
         # Create todos table
-        self.cursor.execute("CREATE TABLE if not exists todos(todoID SERIAL PRIMARY KEY, dateID VARCHAR(255), timestamp VARCHAR(255), completed int, todoItem VARCHAR(255), userID int REFERENCES users)")
+        self.cursor.execute("CREATE TABLE if not exists todos(todoID integer PRIMARY KEY AUTOINCREMENT, dateID VARCHAR(255), timestamp VARCHAR(255), completed int, todoItem VARCHAR(255), userID INTEGER REFERENCES users(userID))")
 
         # Create colors table
-        self.cursor.execute("CREATE TABLE if not exists colors(style VARCHAR(255), userID int REFERENCES users)")
+        self.cursor.execute("CREATE TABLE if not exists colors(style VARCHAR(255), userID INTEGER REFERENCES users(userID))")
 
         # Create theme table
         self.cursor.execute("CREATE TABLE if not exists theme(primary_palette VARCHAR(255), accent_palette VARCHAR(255), theme_style VARCHAR(255))")
 
-        self.cursor.execute("INSERT INTO users (email, password, firstName, lastName) VALUES (%s, %s, %s, %s)", ("mai5013@calu.edu", "Bunnies", "Hailey", "Maimone"))
+        self.cursor.execute("INSERT INTO users (email, password, firstName, lastName) VALUES (?, ?, ?, ?)", ("mai5013@calu.edu", "Bunnies", "Hailey", "Maimone"))
 
         self.conn.commit()
 
     def login(self, userText, passText):
         loginCode = -1
 
-        c = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        query = "SELECT * FROM users WHERE email = %s"
+        c = self.conn.cursor()
+        query = "SELECT * FROM users WHERE email = ?"
         c.execute(query, (userText,))
         records = c.fetchone()
         
@@ -63,7 +55,7 @@ class Database:
 
     def register(self, firstName, lastName, enterPass, passReEnter, emailPrompt):
         regCode = -1
-        c = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        c = self.conn.cursor()
 
         if firstName == "" or lastName == "":
             regCode = -1
@@ -83,18 +75,32 @@ class Database:
                 if record[1] == emailPrompt:
                     regCode = 0
                 else:
-                    c.execute("INSERT INTO users (email, password, firstName, lastName) VALUES (%s, %s, %s, %s)", (emailPrompt, enterPass, firstName, lastName))
+                    c.execute("INSERT INTO users (email, password, firstName, lastName) VALUES (?, ?, ?, ?)", (emailPrompt, enterPass, firstName, lastName))
                     regCode = 1
                     config.store.put('account', userid=record[0], email=emailPrompt, password=enterPass)
                     break
         else:
-            c.execute("INSERT INTO users (email, password, firstName, lastName) VALUES (%s, %s, %s, %s)", (emailPrompt, enterPass, firstName, lastName))
+            c.execute("INSERT INTO users (email, password, firstName, lastName) VALUES (?, ?, ?, ?)", (emailPrompt, enterPass, firstName, lastName))
             regCode = 1
             config.store.put('account', userid=record[0], email=emailPrompt, password=enterPass)
         
         self.conn.commit()
         
         return regCode
+    
+    def update_sticker(self, text, event_text):
+        c = self.conn.cursor()
+        query = "UPDATE events SET sticker = ? WHERE messageBody = ?"
+        c.execute(query, (text, event_text))
+        
+        self.conn.commit()
+
+    def save_stickerColor(self, color, event_text):
+        c = self.conn.cursor()
+        query = "UPDATE events SET color = ? WHERE messageBody = ?"
+        c.execute(query, (color, event_text))
+
+        self.conn.commit()
 
     def close_db(self):
         self.conn.close()
